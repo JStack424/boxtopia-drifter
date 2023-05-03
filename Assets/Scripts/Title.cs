8e5f40db069d80176ef4cb2c955598a9cade19c8
JStack424 <Jstack424@gmail.com>
2023-05-03 13:48:14 -0400

    Controller support for title screen

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -13,13 +13,4 @@ public class Title : MonoBehaviour
         SceneManager.LoadScene("Instructions");
     }
 
-    private void OnExtra()
-    {
-        ViewInstructions();
-    }
-    private void OnStart()
-    {
-        StartGame();
-    }
-
 }
